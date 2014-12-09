359429378b683f76f3d12c2a516f36988b9990e0
Neel Shah <neel@shah7.com>
2014-12-08 19:49:28 -0800

    Revert to b83b024

--- src/components/interface/frt/frt.h
+++ src/components/interface/frt/frt.h
@@ -1,34 +1,13 @@
 #ifndef FRT_H
 #define FRT_H
 
-int frt_vSemaphoreBinaryCreate(void);
-int frt_xSemaphoreTake(int xSemaphore, int xTicksToWait);
-int frt_xSemaphoreGive(int xSemaphore);
-int frt_xTaskCreate(const char * const pcName, unsigned short usStackDepth, void *pvParameters, int uxPriority);
-void frt_vTaskDelete(int xTask);
-void frt_vTaskSuspend(int xTaskToSuspend);
-void frt_vTaskDelay(int xTicksToDelay);
-//void frt_vTaskDelayUntil(int *pxPreviousWakeTime, int xTimeIncrement);
-int frt_vTaskResume(int xTaskToResume);
-int frt_xTaskResumeFromISR(int xTaskToResume);
-//void frt_uxTaskGetSystemState(...);
-int frt_xTaskGetCurrentTaskHandle(void);
-int frt_xTaskGetIdleTaskHandle(void);
-unsigned int frt_uxTaskGetStackHighWaterMark(int xTask);
-//eTaskState frt_eTaskGetState(int xTask);
-//char* frt_pcTaskGetName(int xTaskToQuery);
-//void frt_vTaskGetRunTimeStats(char *pcWriteBuffer);
-//void frt_vTaskSetApplicationTaskTag(int xTask, TODO pxTagValue);
-//TODO frt_xTaskGetApplicationTaskTag(int xTask);
-//int frt_xTaskCallApplicationTaskHook(int xTask, void *pvParameter);
-int frt_xTaskGetTickCount(void);
-int frt_xTaskGetTickCountFromISR(void);
-int frt_xTaskGetSchedulerState(void);
-unsigned int frt_uxTaskGetNumberOfTasks(void);
-//void frt_vTaskList(char *pcWriteBuffer);
-//void frt_vTaskStartTrace(char *pcBuffer, unsigned long ulBufferSize);
-unsigned long frt_ulTaskEndTrace(void);
-int frt_xQueueCreate(int uxQueueLength, int uxItemSize, int ptr, spdid_t spd);
-int frt_xQueueSend(int xQueue, int item, int xTicksToWait);
-int frt_xQueueReceive(int xQueue, int buff, int xTicksToWait);
+extern int frt_vSemaphoreBinaryCreate(void);
+extern int frt_xSemaphoreTake(int xSemaphore, int xTicksToWait);
+extern int frt_xSemaphoreGive(int xSemaphore);
+extern int frt_xTaskCreate(const char * const pcName, unsigned short usStackDepth, void *pvParameters, int uxPriority);
+extern void frt_vTaskDelete(int xTask);
+extern void frt_vTaskSuspend(int xTaskToSuspend);
+extern int frt_xQueueCreate(int uxQueueLength, int uxItemSize, int ptr, spdid_t spd);
+extern int frt_xQueueSend(int xQueue, int item, int xTicksToWait);
+extern int frt_xQueueReceive(int xQueue, int buff, int xTicksToWait);
 #endif	/* FRT_H */
